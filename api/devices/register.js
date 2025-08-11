60c9369ac6e7aa968a2f9e5cdae0c3a4aee05279
zappi <zappi@groupevent.com>
2025-08-11 16:12:11 -0700

    Remove database imports to isolate function invocation issue

--- api/devices/register.js
+++ api/devices/register.js
@@ -1,11 +1,3 @@
-const { neon } = require('@neondatabase/serverless');
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is required');
-}
-
-const sql = neon(process.env.DATABASE_URL);
-
 module.exports = async function handler(req, res) {
   // Enable CORS for all origins
   res.setHeader('Access-Control-Allow-Origin', '*');
